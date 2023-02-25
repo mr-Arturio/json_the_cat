8e2dfdd91218a64f4dc0e214733ce8ba9fa7a813
ArturT <mr.arturio@gmail.com>
2023-02-24 23:38:09 -0500

    adding code comments

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,18 @@
 const request = require('request');
 
-const arg = process.argv.slice(2);
-//console.log(process.argv);
-//console.log(arg[0]);
-const breed = arg[0];
+const arg = process.argv.slice(2); //retrieves command-line arguments passed to the program, excluding the first two arguments
+//console.log(process.argv); // test
+//console.log(arg[0]); // test
+const breed = arg[0]; //the first command-line argument passed to the program (cat breed in our case)
 
 const URL = ` https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(URL, (error, response, body) => {
+request(URL, (error, response, body) => { //request() method takes two parameters: the URL to request, and a callback function (error, response, and body.) to execute when the response is received.
   if (error) {
     //if an error occurs, we output an error message with the error details using console.error()
     console.error('Error occurred:', error);
   } else {
-    const data = JSON.parse(body);
+    const data = JSON.parse(body); // string to an object
     if (data.length === 0) {
       console.log(`Breed '${breed}' not found.`);
     } else {
